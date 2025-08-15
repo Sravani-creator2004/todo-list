f78318008480b38830b62da57d8a417600266ae8
Sravani <sravanikandula27@gmail.com>
2025-08-15 17:40:03 +0930

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const addBtn = document.getElementById('addBtn');
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+
+addBtn.addEventListener('click', () => {
+const taskText = taskInput.value.trim();
+if(taskText !== '') {
+const li = document.createElement('li');
+li.textContent = taskText;
+
+const deleteBtn = document.createElement('button');
+deleteBtn.textContent = 'Delete';
+deleteBtn.addEventListener('click', () => {
+taskList.removeChild(li);
+});
+
+li.appendChild(deleteBtn);
+taskList.appendChild(li);
+taskInput.value = '';
+}
+})
+
+ 
